6d9825ae2ed6cbd71bb9ffa0f64dffc5797b67b2
Shane Jennings <Shane Jennings>
2016-02-06 10:53:04 +0000

    first commit

---
+++ src/resource.js
@@ -0,0 +1,10 @@
+var res = {
+    background_png : "res/Background.png",
+	sprite_png : "res/character.png",
+	pipe_png : "res/Pipe.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
